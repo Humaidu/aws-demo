4fde0377115d3754290dc3579ba45ecd88d79d46
Humaidu <humaiduali@gmail.com>
2024-11-13 13:37:31 +0000

    removed eksctl from the installation

--- jenkins_docker_installation.sh
+++ jenkins_docker_installation.sh
@@ -6,8 +6,14 @@ sudo apt install default-jre -y
 sudo apt install default-jdk -y
 
 #Installing Jenkins
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+# sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \ /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt update
 sudo apt install jenkins -y
 
@@ -42,4 +48,5 @@ sudo apt install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker jenkins
+
 
